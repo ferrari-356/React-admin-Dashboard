e295e791ebd489d17bcd1128a8ab9f6cdd7d4866
William Machado <ferrari-356@hotmail.com>
2021-03-21 22:12:31 -0300

    Initial Commit Project created and update repo

--- src/App.js
+++ src/App.js
@@ -1,17 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-
       <p>
-        In this series we are going to learn building entire dashboard uing Rect & Material design & Vuexy
-        </p>
-
-
-
-
+        In this series we are going to learn building entire dashboard uing Rect
+        & Material design & Vuexy+
+        Thanks for watching 
+      </p>
     </div>
   );
 }
